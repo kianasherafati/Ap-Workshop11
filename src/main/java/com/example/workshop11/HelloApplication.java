627760eb3183e9ea8513597a86ba569ba833d9f1
kiana <kiana.sherafati@gmail.com>
2023-06-06 00:19:26 +0330

    feat : completed HelloApplication class

--- src/main/java/com/example/workshop11/HelloApplication.java
+++ src/main/java/com/example/workshop11/HelloApplication.java
@@ -1,5 +1,4 @@
 package com.example.workshop11;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,11 +7,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculator!");
         stage.setScene(scene);
         stage.show();
     }
